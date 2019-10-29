5d51d817fa4e37d912fbb123974562745f86b0a8
Thibaut <Michel@DESKTOP-9J3RD28>
2019-10-29 19:23:05 +0100

    Task

---
+++ src/main/java/acme/entities/requestss/Requests.java
@@ -0,0 +1,44 @@
+
+package acme.entities.requestss;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Requests extends DomainEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	private String				title;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private Date				moment;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				deadline;
+
+	private String				text;
+
+	@NotBlank
+	private String				reward;
+
+	@NotBlank
+	@Pattern(regexp = "^R[a-zA-Z]{4}-[0-9]{5}$")
+	private String				ticker;
+}
--- src/main/webapp/META-INF/views/master/menu.jsp
+++ src/main/webapp/META-INF/views/master/menu.jsp
@@ -23,6 +23,10 @@
 			<acme:menu-suboption code="master.menu.authenticated.announcement.list" action="/authenticated/announcement/list"/>
 		</acme:menu-option>
 		
+		<acme:menu-option code="master.menu.authenticated.requests" access="isAuthenticated()" >
+			<acme:menu-suboption code="master.menu.authenticated.requests.list" action="/authenticated/requests/list"/>
+		</acme:menu-option>
+		
 		<acme:menu-option code="master.menu.anonymous.announcement" access="isAnonymous()" >
 			<acme:menu-suboption code="master.menu.anonymous.announcement.list" action="/anonymous/announcement/list"/>
 		</acme:menu-option>
