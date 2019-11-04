d472275f2f956db8f76a2aa734b149738383520f
Thibaut <Michel@DESKTOP-9J3RD28>
2019-11-04 13:34:23 +0100

    Task7 acabada

--- src/main/java/acme/entities/requestss/Requests.java
+++ src/main/java/acme/entities/requestss/Requests.java
@@ -7,7 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.Min;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -41,7 +41,7 @@ public class Requests extends DomainEntity {
 	private String				text;
 
 	@NotNull
-	@Min(0)
+	@Valid
 	private Money				reward;
 
 	@NotBlank
